da16ed5be7e74808f3b001534ea3af788bcd2399
calimanis-ygor <ygoralvescalimanis@hotmail.com>
2024-01-15 16:33:45 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+print("""
+╭━━━╮╱╱╭╮╱╱╱╱╱╱╱╭━━━╮
+┃╭━╮┃╱╱┃┃╱╱╱╱╱╱╱┃╭━━╯
+┃╰━━┳━━┫╰━┳━━┳━╮┃╰━━┳╮╭┳━━┳━┳━━┳━━┳━━╮
+╰━━╮┃╭╮┃╭╮┃╭╮┃╭╯┃╭━━┻╋╋┫╭╮┃╭┫┃━┫━━┫━━┫
+┃╰━╯┃╭╮┃╰╯┃╰╯┃┃╱┃╰━━┳╋╋┫╰╯┃┃┃┃━╋━━┣━━┃
+╰━━━┻╯╰┻━━┻━━┻╯╱╰━━━┻╯╰┫╭━┻╯╰━━┻━━┻━━╯
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱┃┃
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰╯
+""")
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = int(input('Escolha uma opção: '))
+print(f'Você escolheu a opção {opcao_escolhida}')
+print(type(opcao_escolhida))
+
+if(opcao_escolhida == 1):
+    nome_restaurante = input('Digite o nome do restaurante: \n')
+    print(f'O restaurante {nome_restaurante} foi cadastrado com sucesso')
+elif (opcao_escolhida == 2):
+     print('Listar restaurantes')
+elif (opcao_escolhida == 3):
+     print('Listar restaurantes')
+else:
+     print('Encerrando o programa!')
